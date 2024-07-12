4936c371fb0bcb5c2764d62e182884ace4b4c12d
PAULO ARRUDA <paulo.arruda@masterboi.com.br>
2024-07-12 16:51:26 -0300

    Aula 03 - Início

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from http import HTTPStatus
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI()
 
+database = []
 
 @app.get('/', status_code = HTTPStatus.OK, response_model = Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.post('/users/', response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,4 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class Message(BaseModel):
     message: str
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
